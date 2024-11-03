bef39d9ee5ed81c671452f26073b7efaae658d03
lisivka <lisivka@gmail.com>
2024-11-03 20:46:10 +0000

    add search, pivot, calendar

--- barge_load/models/barge_load_act.py
+++ barge_load/models/barge_load_act.py
@@ -1,6 +1,8 @@
 import logging
 
-from odoo import models, fields, api
+from datetime import datetime, timedelta
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 _logger = logging.getLogger(__name__)
@@ -15,30 +17,23 @@ class BargeLoadingAct(models.Model):
     date = fields.Date(tracking=True)
 
     quarry_id = fields.Many2one('barge.load.quarry', tracking=True)
-    # owner_quarry_id = fields.Many2one(
-    #     'res.partner',
-    #     compute="_compute_owner_quarry_id",
-    #     store=True,
-    #     tracking=True
-    # )
-    owner_quarry_id = fields.Many2one('res.partner', string='Owner Quarry',
-                                      compute='_compute_owner_quarry_id',
-                                      store=True)
+    owner_quarry_id = fields.Many2one('res.partner', tracking=True)
 
-    receiver_id = fields.Many2one('res.partner', tracking=True)
+    receiver_id = fields.Many2one('res.partner', copy=False,
+                                  tracking=True)
 
-    dredger_agent_id = fields.Many2one('res.partner', tracking=True)
-    tugboat_agent_id = fields.Many2one('res.partner', tracking=True)
+    dredger_agent_id = fields.Many2one('res.partner', copy=False,
+                                       tracking=True)
+    tugboat_agent_id = fields.Many2one('res.partner', copy=False,
+                                       tracking=True)
 
     dredger_id = fields.Many2one('barge.load.dredger', tracking=True)
-    barge_id = fields.Many2one('barge.load.barge', tracking=True)
-    tugboat_id = fields.Many2one('barge.load.tugboat', tracking=True)
-    cargo_carrier_id = fields.Many2one(
-        'res.partner',
-        compute="_compute_cargo_carrier_id",
-        store=True,
-        tracking=True
-    )
+    barge_id = fields.Many2one('barge.load.barge', copy=False,
+                               tracking=True)
+    tugboat_id = fields.Many2one('barge.load.tugboat', copy=False,
+                                 tracking=True)
+    cargo_carrier_id = fields.Many2one('res.partner', copy=False,
+                                       tracking=True)
 
     status = fields.Selection(
         selection=[
@@ -51,29 +46,41 @@ class BargeLoadingAct(models.Model):
     )
 
     # Work Time
-    start_time = fields.Datetime(tracking=True)
-    end_time = fields.Datetime(tracking=True)
+    start_time = fields.Datetime(copy=False, tracking=True)
+    end_time = fields.Datetime(copy=False, tracking=True)
     duration = fields.Float(
         compute="_compute_duration",
         string="Loading Duration (h)",
     )
 
     # ----- START ship`s draft
-    before_left_nose = fields.Float(string="Before Left Nose (cm)")
-    before_left_mid = fields.Float(string="Before Left Mid (cm)")
-    before_left_stern = fields.Float(string="Before Left Stern (cm)")
+    before_left_nose = fields.Float(string="Before Left Nose (cm)",
+                                    copy=False, tracking=True, )
+    before_left_mid = fields.Float(string="Before Left Mid (cm)",
+                                    copy=False, tracking=True, )
+    before_left_stern = fields.Float(string="Before Left Stern (cm)",
+                                    copy=False, tracking=True, )
 
-    before_right_nose = fields.Float(string="Before Right Nose (cm)")
-    before_right_mid = fields.Float(string="Before Right Mid (cm)")
-    before_right_stern = fields.Float(string="Before Right Stern (cm)")
+    before_right_nose = fields.Float(string="Before Right Nose (cm)",
+                                    copy=False, tracking=True, )
+    before_right_mid = fields.Float(string="Before Right Mid (cm)",
+                                    copy=False, tracking=True, )
+    before_right_stern = fields.Float(string="Before Right Stern (cm)",
+                                    copy=False, tracking=True, )
 
-    after_left_nose = fields.Float(string="After Left Nose (cm)")
-    after_left_mid = fields.Float(string="After Left Mid (cm)")
-    after_left_stern = fields.Float(string="After Left Stern (cm)")
+    after_left_nose = fields.Float(string="After Left Nose (cm)",
+                                    copy=False, tracking=True, )
+    after_left_mid = fields.Float(string="After Left Mid (cm)",
+                                    copy=False, tracking=True, )
+    after_left_stern = fields.Float(string="After Left Stern (cm)",
+                                    copy=False, tracking=True, )
 
-    after_right_nose = fields.Float(string="After Right Nose (cm)")
-    after_right_mid = fields.Float(string="After Right Mid (cm)")
-    after_right_stern = fields.Float(string="After Right Stern (cm)")
+    after_right_nose = fields.Float(string="After Right Nose (cm)",
+                                    copy=False, tracking=True, )
+    after_right_mid = fields.Float(string="After Right Mid (cm)",
+                                    copy=False, tracking=True, )
+    after_right_stern = fields.Float(string="After Right Stern (cm)",
+                                    copy=False, tracking=True, )
 
     before_avg_draft = fields.Float(
         compute="_compute_before_avg_draft",
@@ -83,60 +90,64 @@ class BargeLoadingAct(models.Model):
     after_avg_draft = fields.Float(
         compute="_compute_after_avg_draft",
         string="After Avg Draft (cm)",
-         digits=(6, 3),
+        digits=(6, 3),
     )
 
     draft_difference = fields.Float(
         compute="_compute_draft_difference",
         string="Difference of Average Draft Before and After Loading (cm)",
-        digits=(6, 3)
+        digits=(6, 3),
+        store=True
     )
     # ----- END ship`s draft
 
-    # ----- START cargo capacity
+    # ----- START calculated weight
     cargo_capacity_per_cm = fields.Float(
         related='barge_id.cargo_capacity_per_cm',
         string="Average Cargo Capacity per cm",
         help="Average Cargo Capacity per cm "
              "(as per registration documents or calibration certificate)",
         readonly=True,
-        tracking=True
+        store=True,
+        tracking=True,
     )
-
     calculated_weight = fields.Float(
         compute="_compute_calculated_weight",
         string="Calculated Weight (t)",
         help="Calculated Weight of Water-Saturated Sand (t)",
-        digits=(6, 3))
+        digits=(6, 3),
+        store=True,
+        tracking=True,
+
+    )
     moisture_coefficient = fields.Float(
         string="Moisture Coefficient (%)",
         help="Moisture Coefficient for Determining Recorded Weight (%)",
         default=22,
+        store=True,
         digits=(3, 1),
+
     )
     record_weight = fields.Float(
         compute="_compute_record_weight",
         string="Record Weight (t)",
         help="Record Weight of Water-Saturated Sand (t)",
         digits=(6, 3),
-
+        store=True,
+        tracking=True
     )
 
-    @api.depends('quarry_id')
-    def _compute_owner_quarry_id(self):
-        for record in self:
-            if record.quarry_id:
-                record.owner_quarry_id = record.quarry_id.owner_id
-            else:
-                record.owner_quarry_id = None
+    # ----- END calculated weight
 
-    @api.depends('tugboat_id')
-    def _compute_cargo_carrier_id(self):
-        for record in self:
-            if record.tugboat_id:
-                record.cargo_carrier_id = record.tugboat_id.owner_id
-            else:
-                record.cargo_carrier_id = None
+    @api.onchange('quarry_id')
+    def _onchange_quarry_id(self):
+        if self.quarry_id:
+            self.owner_quarry_id = self.quarry_id.owner_id
+
+    @api.onchange('tugboat_id')
+    def _onchange_cargo_carrier_id(self):
+        if self.tugboat_id:
+            self.cargo_carrier_id = self.tugboat_id.owner_id
 
     @api.depends('start_time', 'end_time')
     def _compute_duration(self):
@@ -180,12 +191,50 @@ class BargeLoadingAct(models.Model):
     @api.depends('draft_difference', 'cargo_capacity_per_cm')
     def _compute_calculated_weight(self):
         for record in self:
-            record.calculated_weight = (record.draft_difference *
-                                        record.cargo_capacity_per_cm)
+            if record.draft_difference and record.cargo_capacity_per_cm:
+                record.calculated_weight = record.draft_difference * record.cargo_capacity_per_cm
 
-    #
     @api.depends('calculated_weight', 'moisture_coefficient')
     def _compute_record_weight(self):
         for record in self:
             record.record_weight = record.calculated_weight * (
                     1 - record.moisture_coefficient / 100)
+
+    @api.model
+    def write(self, vals):
+
+        """
+        Override standard write method to prevent modification of fields other
+        than 'status' or 'id' when the act status is set to 'Done'.
+
+        """
+
+        for record in self:
+            if record.status == 'done':
+                restricted_fields = [field for field in vals.keys() if
+                                     field not in ['id', 'status']]
+
+                if restricted_fields:
+                    raise ValidationError(
+                        _("You cannot modify  act - the status is 'Done'.")
+                    )
+
+        # Call base write method if checks passed
+        return super(BargeLoadingAct, self).write(vals)
+
+    last_7_days_date = fields.Date(compute='_compute_dates', store=False)
+    last_30_days_date = fields.Date(compute='_compute_dates', store=False)
+
+    @api.depends(
+        'date')  # Залежність на випадок, якщо вам потрібно буде оновити ці поля
+    def _compute_dates(self):
+        for record in self:
+            record.last_7_days_date = (
+                        datetime.now() - timedelta(days=7)).date()
+            record.last_30_days_date = (
+                        datetime.now() - timedelta(days=30)).date()
+
+    @api.model
+    def default_get(self, fields):
+        res = super(BargeLoadingAct, self).default_get(fields)
+        return res
